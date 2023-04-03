c04d9c8ec2f84547f02821c3b00c54728da631fc
Deependra-programmers <Deependra.panwar@programmers.io>
2023-04-03 19:20:03 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
+import { QuillConfiguration } from "./quill-configration";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello';
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+  quillConfiguration = QuillConfiguration
+  RichTextForm: FormGroup;
+  constructor(){
+      this.RichTextForm = new FormGroup({
+        textEditor: new FormControl(""),
+      });
+    }
+    testEditorContent(){
+      alert(this.RichTextForm.get('textEditor')!.value);
+    }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {QuillModule} from 'ngx-quill'
+import { ReactiveFormsModule } from "@angular/forms";
+import {MatButtonModule} from'@angular/material/button';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    QuillModule.forRoot(),
+    ReactiveFormsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
